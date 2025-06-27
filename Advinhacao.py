8ad5efc389b8b99280a58b84cd1561aa88ed523c
lauraguilhem <laura.alonso.santos@escola.pr.gov.br>
2025-06-27 09:39:26 -0300

    Add files via upload

---
+++ Advinhacao.py
@@ -0,0 +1,17 @@
+#Laura Guilhem n25 1E
+import random
+
+import os 
+
+erros=0
+sorteado=random.randrange(0,100)
+jogador=int(input("Digite seu número!"))
+while(sorteado!=jogador):
+    os.system('cls')
+    if(sorteado>jogador):
+        print("ERRO, o número é maior")
+    elif(sorteado<jogador):
+        print("ERRO, o número é menor")
+        erros+=1
+        jogador=int(input("Digite seu número:"))
+        print("Número" + str(jogador) + "você acertou em :" + str(erros+1) + "tentativas")
