94683714526927da6ec32c362150339a93762e86
lauraguilhem <laura.alonso.santos@escola.pr.gov.br>
2025-10-09 09:08:10 -0300

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,9 @@
-# calculadora simples
-print("Vamos somar dois números!")
-num1 = float(input("Digite o primeiro número:"))
-num2 = float(input("Digite o segundo número:"))
-soma = num1 + num2
-print(f"A soma de {num1} e {num2} é {soma}")
+print("Vamos multiplicar dois numeros!")
+
+num1 = int(input("Digite o primeiro numero, por favor: "))
+num2 = int(input("Digite o segundo numero, por favor: "))
+multiplica = num1 * num2
+
+
+
+print(f"O resultado da multiplicação é {multiplica}.")
